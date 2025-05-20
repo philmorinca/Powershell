2ba76d00e5a5350138f7e10ddbda284e238ed2fd
philmorinca <philippe.morin.qc.ca@gmail.com>
2025-05-20 18:02:25 -0400

    Add cleanup

--- Uninstall TeamViewer.ps1
+++ Uninstall TeamViewer.ps1
@@ -1,1 +1,2 @@
 & $((Get-Item "C:\Program File*\TeamViewer\Uninstall.exe" | Select-Object -First 1 FullName).FullName) /S
+Get-Item "C:\Program File*\TeamViewer" | Remove-Item -Recurse -Force -Confirm:$false 
