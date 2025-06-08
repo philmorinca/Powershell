144899c3ddccf194f6eac198eb2291cbb0861d81
philmorinca <philippe.morin.qc.ca@gmail.com>
2025-06-08 14:40:37 -0400

    Cleanup

--- Win10LTSC/setupcomplete.ps1
+++ Win10LTSC/setupcomplete.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop'
 Set-ExecutionPolicy Bypass -Scope Process
 
-Get-Item "C:\setupcomplete.cmd" | Remove-Item -Force #-ErrorAction SilentlyContinue
-Get-Item "C:\Win10LTSC.iso" | Remove-Item -Force #-ErrorAction SilentlyContinue
+Remove-Item "C:\setupcomplete.cmd" -Force -ErrorAction SilentlyContinue
+Remove-Item "C:\Win10LTSC.iso" -Force -ErrorAction SilentlyContinue
 
 #Activate windows
 & ([ScriptBlock]::Create((Invoke-RestMethod https://get.activated.win))) /HWID
