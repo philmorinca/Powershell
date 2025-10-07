3e4d76da58e75d56a3668e755fce5025014cb28c
Philippe Morin <philippe.morin.qc.ca@gmail.com>
2025-10-07 10:07:17 -0400

    Software

---
+++ Standard Software.ps1
@@ -0,0 +1,6 @@
+
+winget.exe install --id "Adobe.Acrobat.Reader.64-bit" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --force
+winget.exe install --id "Klocman.BulkCrapUninstaller" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --force
+winget.exe install --id "WinDirStat.WinDirStat" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --force
+winget.exe install --id "VideoLAN.VLC" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --force
+winget.exe install --id "Microsoft.Teams" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --force
