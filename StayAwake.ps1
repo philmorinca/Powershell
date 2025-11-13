53e862e311df48ea1efaa9c08b6d3f17a5c620ae
philmorinca <philippe.morin.qc.ca@gmail.com>
2025-11-13 00:23:29 -0500

    Add download command for StayAwake.ps1
    Added a command to download the script from GitHub.

--- StayAwake.ps1
+++ StayAwake.ps1
@@ -1,3 +1,4 @@
+# iwr https://github.com/philmorinca/Powershell/raw/refs/heads/main/StayAwake.ps1 -UseBasicParsing | iex
 powercfg /x -hibernate-timeout-ac 0
 powercfg /x -disk-timeout-ac 0
 powercfg /x -standby-timeout-ac 0
