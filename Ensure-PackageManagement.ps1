1e75826a6198e4280500422b8b59dd1ca4a9e7a5
philmorinca <philippe.morin.qc.ca@gmail.com>
2026-01-28 19:33:12 -0500

    Add script to ensure PackageManagement installation

---
+++ Ensure-PackageManagement.ps1
@@ -0,0 +1,26 @@
+# URL of the package
+$Url = "https://cdn.powershellgallery.com/packages/packagemanagement.1.4.8.1.nupkg"
+
+# Where to download the file
+$DownloadPath = "$env:TEMP\packagemanagement.1.4.8.1.nupkg"
+$ZipPath = "$DownloadPath.zip"
+# Module installation directory
+$ModulePath = "$env:ProgramFiles\WindowsPowerShell\Modules\PackageManagement\1.4.8.1"
+
+# Create module directory if missing
+if (!(Test-Path $ModulePath)) {
+    New-Item -ItemType Directory -Path $ModulePath -Force | Out-Null
+}
+
+# Download the package
+Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
+Move-Item $DownloadPath $ZipPath
+# Extract the .nupkg (it's a ZIP file)
+Expand-Archive -Path $ZipPath -DestinationPath $ModulePath -Force
+
+# Remove the downloaded file
+Remove-Item $ZipPath -Force
+
+# Update module cache
+$null = Get-Module -ListAvailable | Out-Null
+
